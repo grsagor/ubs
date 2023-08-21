e791f3cdb15ef36b60848ef647857249a987e684
Golam Rahman Sagor <grsagor08@gmail.com>
2023-08-21 23:38:45 +0600

    starting 21 aug

--- Modules/Superadmin/Resources/views/superadmin/index.blade.php
+++ Modules/Superadmin/Resources/views/superadmin/index.blade.php
@@ -107,6 +107,20 @@
 	            <a href="{{action([\Modules\Superadmin\Http\Controllers\BusinessController::class, 'index'])}}" class="small-box-footer">@lang('superadmin::lang.more_info') <i class="fa fa-arrow-circle-right"></i></a>
 	          </div>
 	        </div>
+	        <div class="col-lg-4 col-xs-6">
+	          <!-- small box -->
+	          <div class="small-box bg-red">
+	            <div class="inner">
+	              <h3></h3>
+
+	              <p>Service Charges</p>
+	            </div>
+	            <div class="icon">
+	              <i class="ion ion-pie-graph"></i>
+	            </div>
+	            <a href="" class="small-box-footer">@lang('superadmin::lang.more_info') <i class="fa fa-arrow-circle-right"></i></a>
+	          </div>
+	        </div>
         	<!-- ./col -->
     	</div>
 
