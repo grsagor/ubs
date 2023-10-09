fc59ebe224887f64ada6cad61008fc9459fa13a6
Abdulla-Shakib <abshakib91@gmail.com>
2023-10-09 23:41:37 +0600

    Login page header change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.auth2')
 @section('title', __('lang_v1.login'))
 
+
+
+
 @section('content')
+
+    <!-- Register Url -->
+    @if (config('constants.allow_registration'))
+        <div class="btn-group">
+            <button type="button" class="btn dropdown-toggle bg-maroon btn-flat" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">
+                <b>{{ __('business.not_yet_registered') }}</b>
+                {{ __('business.register_now') }} <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+                <li><a href="{{ url('business/register') }}">Register as business</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="{{ url('customer/register') }}">Register as customer</a></li>
+            </ul>
+        </div>
+    @endif
+
     <div class="login-form col-md-12 col-xs-12 right-col-content">
         <p class="form-header text-white">@lang('lang_v1.login')</p>
         <form method="POST" action="{{ route('login') }}" id="login-form">
@@ -10,11 +30,11 @@
                 @php
                     $username = old('username');
                     $password = null;
-                    if(config('app.env') == 'demo'){
+                    if (config('app.env') == 'demo') {
                         $username = 'admin';
                         $password = '123456';
-
-                        $demo_types = array(
+                    
+                        $demo_types = [
                             'all_in_one' => 'admin',
                             'super_market' => 'admin',
                             'pharmacy' => 'admin-pharmacy',
@@ -25,14 +45,15 @@
                             'woocommerce' => 'woocommerce_user',
                             'essentials' => 'admin-essentials',
                             'manufacturing' => 'manufacturer-demo',
-                        );
-
-                        if( !empty($_GET['demo_type']) && array_key_exists($_GET['demo_type'], $demo_types) ){
+                        ];
+                    
+                        if (!empty($_GET['demo_type']) && array_key_exists($_GET['demo_type'], $demo_types)) {
                             $username = $demo_types[$_GET['demo_type']];
                         }
                     }
                 @endphp
-                <input id="username" type="text" class="form-control" name="username" value="{{ $username }}" required autofocus placeholder="@lang('lang_v1.username')">
+                <input id="username" type="text" class="form-control" name="username" value="{{ $username }}"
+                    required autofocus placeholder="@lang('lang_v1.username')">
                 <span class="fa fa-user form-control-feedback"></span>
                 @if ($errors->has('username'))
                     <span class="help-block">
@@ -41,8 +62,8 @@
                 @endif
             </div>
             <div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
-                <input id="password" type="password" class="form-control" name="password"
-                value="{{ $password }}" required placeholder="@lang('lang_v1.password')">
+                <input id="password" type="password" class="form-control" name="password" value="{{ $password }}"
+                    required placeholder="@lang('lang_v1.password')">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 @if ($errors->has('password'))
                     <span class="help-block">
@@ -60,73 +81,100 @@
             <br>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary btn-flat btn-login">@lang('lang_v1.login')</button>
-                @if(config('app.env') != 'demo')
-                <a href="{{ route('password.request') }}" class="pull-right">
-                    @lang('lang_v1.forgot_your_password')
-                </a>
-            @endif
+                @if (config('app.env') != 'demo')
+                    <a href="{{ route('password.request') }}" class="pull-right">
+                        @lang('lang_v1.forgot_your_password')
+                    </a>
+                @endif
             </div>
         </form>
     </div>
-    @if(config('app.env') == 'demo')
-    <div class="col-md-12 col-xs-12" style="padding-bottom: 30px;">
-        @component('components.widget', ['class' => 'box-primary', 'header' => '<h4 class="text-center">Demo Shops <small><i> Demos are for example purpose only, this application <u>can be used in many other similar businesses.</u></i></small></h4>'])
+    @if (config('app.env') == 'demo')
+        <div class="col-md-12 col-xs-12" style="padding-bottom: 30px;">
+            @component('components.widget', [
+                'class' => 'box-primary',
+                'header' =>
+                    '<h4 class="text-center">Demo Shops <small><i> Demos are for example purpose only, this application <u>can be used in many other similar businesses.</u></i></small></h4>',
+            ])
+                <a href="?demo_type=all_in_one" class="btn btn-app bg-olive demo-login" data-toggle="tooltip"
+                    title="Showcases all feature available in the application." data-admin="{{ $demo_types['all_in_one'] }}">
+                    <i class="fas fa-star"></i> All In One</a>
 
-            <a href="?demo_type=all_in_one" class="btn btn-app bg-olive demo-login" data-toggle="tooltip" title="Showcases all feature available in the application." data-admin="{{$demo_types['all_in_one']}}"> <i class="fas fa-star"></i> All In One</a>
+                <a href="?demo_type=pharmacy" class="btn bg-maroon btn-app demo-login" data-toggle="tooltip"
+                    title="Shops with products having expiry dates." data-admin="{{ $demo_types['pharmacy'] }}"><i
+                        class="fas fa-medkit"></i>Pharmacy</a>
 
-            <a href="?demo_type=pharmacy" class="btn bg-maroon btn-app demo-login" data-toggle="tooltip" title="Shops with products having expiry dates." data-admin="{{$demo_types['pharmacy']}}"><i class="fas fa-medkit"></i>Pharmacy</a>
+                <a href="?demo_type=services" class="btn bg-orange btn-app demo-login" data-toggle="tooltip"
+                    title="For all service providers like Web Development, Restaurants, Repairing, Plumber, Salons, Beauty Parlors etc."
+                    data-admin="{{ $demo_types['services'] }}"><i class="fas fa-wrench"></i>Multi-Service Center</a>
 
-            <a href="?demo_type=services" class="btn bg-orange btn-app demo-login" data-toggle="tooltip" title="For all service providers like Web Development, Restaurants, Repairing, Plumber, Salons, Beauty Parlors etc." data-admin="{{$demo_types['services']}}"><i class="fas fa-wrench"></i>Multi-Service Center</a>
+                <a href="?demo_type=electronics" class="btn bg-purple btn-app demo-login" data-toggle="tooltip"
+                    title="Products having IMEI or Serial number code." data-admin="{{ $demo_types['electronics'] }}"><i
+                        class="fas fa-laptop"></i>Electronics & Mobile Shop</a>
 
-            <a href="?demo_type=electronics" class="btn bg-purple btn-app demo-login" data-toggle="tooltip" title="Products having IMEI or Serial number code."  data-admin="{{$demo_types['electronics']}}" ><i class="fas fa-laptop"></i>Electronics & Mobile Shop</a>
+                <a href="?demo_type=super_market" class="btn bg-navy btn-app demo-login" data-toggle="tooltip"
+                    title="Super market & Similar kind of shops." data-admin="{{ $demo_types['super_market'] }}"><i
+                        class="fas fa-shopping-cart"></i> Super Market</a>
 
-            <a href="?demo_type=super_market" class="btn bg-navy btn-app demo-login" data-toggle="tooltip" title="Super market & Similar kind of shops." data-admin="{{$demo_types['super_market']}}" ><i class="fas fa-shopping-cart"></i> Super Market</a>
+                <a href="?demo_type=restaurant" class="btn bg-red btn-app demo-login" data-toggle="tooltip"
+                    title="Restaurants, Salons and other similar kind of shops."
+                    data-admin="{{ $demo_types['restaurant'] }}"><i class="fas fa-utensils"></i> Restaurant</a>
+                <hr>
 
-            <a href="?demo_type=restaurant" class="btn bg-red btn-app demo-login" data-toggle="tooltip" title="Restaurants, Salons and other similar kind of shops." data-admin="{{$demo_types['restaurant']}}"><i class="fas fa-utensils"></i> Restaurant</a>
-            <hr>
+                <i class="icon fas fa-plug"></i> Premium optional modules:<br><br>
 
-            <i class="icon fas fa-plug"></i> Premium optional modules:<br><br>
+                <a href="?demo_type=superadmin" class="btn bg-red-active btn-app demo-login" data-toggle="tooltip"
+                    title="SaaS & Superadmin extension Demo" data-admin="{{ $demo_types['superadmin'] }}"><i
+                        class="fas fa-university"></i> SaaS / Superadmin</a>
 
-            <a href="?demo_type=superadmin" class="btn bg-red-active btn-app demo-login" data-toggle="tooltip" title="SaaS & Superadmin extension Demo" data-admin="{{$demo_types['superadmin']}}"><i class="fas fa-university"></i> SaaS / Superadmin</a>
+                <a href="?demo_type=woocommerce" class="btn bg-woocommerce btn-app demo-login" data-toggle="tooltip"
+                    title="WooCommerce demo user - Open web shop in minutes!!" style="color:white !important"
+                    data-admin="{{ $demo_types['woocommerce'] }}"> <i class="fab fa-wordpress"></i> WooCommerce</a>
 
-            <a href="?demo_type=woocommerce" class="btn bg-woocommerce btn-app demo-login" data-toggle="tooltip" title="WooCommerce demo user - Open web shop in minutes!!" style="color:white !important" data-admin="{{$demo_types['woocommerce']}}"> <i class="fab fa-wordpress"></i> WooCommerce</a>
-
-            <a href="?demo_type=essentials" class="btn bg-navy btn-app demo-login" data-toggle="tooltip" title="Essentials & HRM (human resource management) Module Demo" style="color:white !important" data-admin="{{$demo_types['essentials']}}">
+                <a href="?demo_type=essentials" class="btn bg-navy btn-app demo-login" data-toggle="tooltip"
+                    title="Essentials & HRM (human resource management) Module Demo" style="color:white !important"
+                    data-admin="{{ $demo_types['essentials'] }}">
                     <i class="fas fa-check-circle"></i>
                     Essentials & HRM</a>
-                    
-            <a href="?demo_type=manufacturing" class="btn bg-orange btn-app demo-login" data-toggle="tooltip" title="Manufacturing module demo" style="color:white !important" data-admin="{{$demo_types['manufacturing']}}">
+
+                <a href="?demo_type=manufacturing" class="btn bg-orange btn-app demo-login" data-toggle="tooltip"
+                    title="Manufacturing module demo" style="color:white !important"
+                    data-admin="{{ $demo_types['manufacturing'] }}">
                     <i class="fas fa-industry"></i>
                     Manufacturing Module</a>
 
-            <a href="?demo_type=superadmin" class="btn bg-maroon btn-app demo-login" data-toggle="tooltip" title="Project module demo" style="color:white !important" data-admin="{{$demo_types['superadmin']}}">
+                <a href="?demo_type=superadmin" class="btn bg-maroon btn-app demo-login" data-toggle="tooltip"
+                    title="Project module demo" style="color:white !important" data-admin="{{ $demo_types['superadmin'] }}">
                     <i class="fas fa-project-diagram"></i>
                     Project Module</a>
 
-            <a href="?demo_type=services" class="btn btn-app demo-login" data-toggle="tooltip" title="Advance repair module demo" style="color:white !important; background-color: #bc8f8f" data-admin="{{$demo_types['services']}}">
+                <a href="?demo_type=services" class="btn btn-app demo-login" data-toggle="tooltip"
+                    title="Advance repair module demo" style="color:white !important; background-color: #bc8f8f"
+                    data-admin="{{ $demo_types['services'] }}">
                     <i class="fas fa-wrench"></i>
                     Advance Repair Module</a>
 
-            <a href="{{url('docs')}}" target="_blank" class="btn btn-app" data-toggle="tooltip" title="Advance repair module demo" style="color:white !important; background-color: #2dce89">
+                <a href="{{ url('docs') }}" target="_blank" class="btn btn-app" data-toggle="tooltip"
+                    title="Advance repair module demo" style="color:white !important; background-color: #2dce89">
                     <i class="fas fa-network-wired"></i>
                     Connector Module / API Documentation</a>
-        @endcomponent   
-    </div>
-    @endif 
+            @endcomponent
+        </div>
+    @endif
 @stop
 @section('javascript')
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#change_lang').change( function(){
-            window.location = "{{ route('login') }}?lang=" + $(this).val();
-        });
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#change_lang').change(function() {
+                window.location = "{{ route('login') }}?lang=" + $(this).val();
+            });
 
-        $('a.demo-login').click( function (e) {
-           e.preventDefault();
-           $('#username').val($(this).data('admin'));
-           $('#password').val("{{$password}}");
-           $('form#login-form').submit();
-        });
-    })
-</script>
+            $('a.demo-login').click(function(e) {
+                e.preventDefault();
+                $('#username').val($(this).data('admin'));
+                $('#password').val("{{ $password }}");
+                $('form#login-form').submit();
+            });
+        })
+    </script>
 @endsection
--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -58,30 +58,7 @@
                         </select>
                     </div>
                     <div class="col-md-9 col-xs-8" style="text-align: right;padding-top: 10px;">
-                        @if (
-                            !($request->segment(1) == 'business' && $request->segment(2) == 'register') ||
-                                Request::url() != '/customer/register')
-                            <!-- Register Url -->
-                            @if (config('constants.allow_registration'))
-                                <div class="btn-group">
-                                    <button type="button" class="btn dropdown-toggle bg-maroon btn-flat"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <b>{{ __('business.not_yet_registered') }}</b>
-                                        {{ __('business.register_now') }} <span class="caret"></span>
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="{{ url('business/register') }}">Register as business</a></li>
-                                        <li role="separator" class="divider"></li>
-                                        <li><a href="{{ url('customer/register') }}">Register as customer</a></li>
-                                    </ul>
-                                </div>
-                                {{-- <a href="{{ route('business.getRegister') }}@if (!empty(request()->lang)){{'?lang=' . request()->lang}} @endif" class="btn bg-maroon btn-flat" ><b>{{ __('business.not_yet_registered')}}</b> {{ __('business.register_now') }}</a> --}}
-                                <!-- pricing url -->
-                                @if (Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
-                                    {{-- &nbsp; <a href="{{ action([\Modules\Superadmin\Http\Controllers\PricingController::class, 'index']) }}">@lang('superadmin::lang.pricing')</a> --}}
-                                @endif
-                            @endif
-                        @endif
+
                         @if ($request->segment(1) != 'login')
                             &nbsp; &nbsp;<span class="text-white">{{ __('business.already_registered') }} </span><a
                                 href="{{ action([\App\Http\Controllers\Auth\LoginController::class, 'login']) }}@if (!empty(request()->lang)) {{ '?lang=' . request()->lang }} @endif">{{ __('business.sign_in') }}</a>
