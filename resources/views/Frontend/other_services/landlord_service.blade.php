ef4781a7f63fe033944ef617d296689286f6ea61
Abdulla-Shakib <abshakib91@gmail.com>
2023-10-27 00:46:30 +0600

    Digital marketing

--- resources/views/Frontend/other_services/landlord_service.blade.php
+++ resources/views/Frontend/other_services/landlord_service.blade.php
@@ -27,7 +27,7 @@
         <div class="header m-3 p-3">
 
             <div class="welcome">
-                <p style="font-size: 16px;">
+                <p style="font-size: 16px;" class="fw-bold">
                     Unipuller, as a dedicated management consultancy firm, is committed to simplifying the landlord-tenant
                     connection process. We understand the unique needs of landlords and are devoted to making your
                     experience as seamless and stress-free as possible. Here's an in-depth look at the comprehensive
@@ -36,106 +36,66 @@
                 </p>
             </div>
 
-            <div class="discover mt-4">
-                <h5 class="fw-bold"> Service Introduction </h5>
+            <div class="why_choose_uit mt-4">
+                <h4 class="fw-bold">
+                    Service Introduction
+                </h4>
                 <div class="details">
-                    <div class="mt-2">
-                        <div class="font-black margin_left_45">
-                            <b>Free Tenant Referral Services</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        Our complimentary tenant referral services are designed to connect
-                                        you with potential tenants who are a perfect fit for your property. We understand
-                                        the importance of finding the right tenants to maintain the integrity of your
-                                        property.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                    <div class="margin_left_45">
+                        <p>
+                            <b>Free Tenant Referral Services:</b> Our complimentary tenant referral services are designed to
+                            connect
+                            you with potential tenants who are a perfect fit for your property. We understand
+                            the importance of finding the right tenants to maintain the integrity of your
+                            property.
+                        </p>
+                        <p>
+                            <b>No Fees, No Hassles:</b> Unipuller never imposes any fees on landlords. Your property is your
+                            priority, and we ensure you can focus on it without any financial burden.
+                        </p>
+                        <p>
+                            <b>Respect for Privacy:</b> We hold your privacy and specific requirements in the highest
+                            regard. Your needs are
+                            central to our mission, and we handle them with the utmost care and confidentiality.
+                        </p>
 
-                        <div class="font-black margin_left_45 mt-2">
-                            <b>No Fees, No Hassles</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        Unipuller never imposes any fees on landlords. Your property is your
-                                        priority, and we ensure you can focus on it without any financial burden.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                    </div>
 
-                        <div class="font-black margin_left_45 mt-2">
-                            <b>Respect for Privacy</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        We hold your privacy and specific requirements in the highest regard. Your needs are
-                                        central to our mission, and we handle them with the utmost care and confidentiality.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                </div>
+            </div>
+
+            <div class="why_choose_uit mt-4">
+                <h4 class="fw-bold">
+                    Complementary Services for a Seamless Experience
+                </h4>
+                <div class="details">
+                    <div class="margin_left_45">
+                        <p>
+                            <b>Viewings Arrangements</b> We can seamlessly organize property viewings at no cost to you.
+                            This convenience
+                            ensures that potential tenants can easily explore your property.
+                        </p>
+                        <p>
+                            <b>Online Property Advertising:</b> Your property will receive prominent visibility on our
+                            website. This online presence
+                            makes it effortless for potential tenants to discover and book viewings, increasing
+                            your property's exposure.
+                        </p>
+                        <p>
+                            <b>Emailing Booking Information:</b> Unipuller provides complete booking information directly to
+                            your email. This keeps
+                            you informed at all times and simplifies communication with potential tenants.
+                        </p>
+                        <p>
+                            <b>Incentives for Landlords:</b> When you entrust Unipuller to manage the entire rental process,
+                            you'll receive
+                            financial incentives ranging from £50 to £300. It's our way of showing appreciation
+                            for your trust in our services.
+                        </p>
                     </div>
                 </div>
             </div>
 
-            <div class="discover mt-4">
-                <h5 class="fw-bold"> Complementary Services for a Seamless Experience</h5>
-                <div class="details">
-                    <div class="mt-2">
-                        <div class="font-black margin_left_45">
-                            <b>Viewings Arrangements</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        We can seamlessly organize property viewings at no cost to you. This convenience
-                                        ensures that potential tenants can easily explore your property.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="font-black margin_left_45 mt-2">
-                            <b>Online Property Advertising</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        Your property will receive prominent visibility on our website. This online presence
-                                        makes it effortless for potential tenants to discover and book viewings, increasing
-                                        your property's exposure.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="font-black margin_left_45 mt-2">
-                            <b>Emailing Booking Information</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        Unipuller provides complete booking information directly to your email. This keeps
-                                        you informed at all times and simplifies communication with potential tenants.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="font-black margin_left_45 mt-2">
-                            <b>Incentives for Landlords</b>
-                            <div class="sub-details mt-1 margin_left_45">
-                                <ul>
-                                    <li class="mt-1">
-                                        When you entrust Unipuller to manage the entire rental process, you'll receive
-                                        financial incentives ranging from £50 to £300. It's our way of showing appreciation
-                                        for your trust in our services.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <div class="discover mt-4">
                 <h5 class="fw-bold"> Additional Partner Referral Services</h5>
@@ -199,7 +159,6 @@
                             flows seamlessly and effortlessly. Your satisfaction is our priority, and we're here to support
                             you every step of the way. Choose Unipuller as your trusted partner in the landlord-tenant
                             connection process.
-
                         </p>
                     </div>
                 </div>
