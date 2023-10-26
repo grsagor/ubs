e7a203b6d321a5e2b43a5c6f9df62556b42ce193
Abdulla-Shakib <abshakib91@gmail.com>
2023-10-27 01:08:29 +0600

    Partner boarding

--- resources/views/Frontend/other_services/partner_boarding.blade.php
+++ resources/views/Frontend/other_services/partner_boarding.blade.php
@@ -1,37 +1,102 @@
 @extends('frontend.layouts.master_layout')
+@section('css')
+    <style>
+        p,
+        li {
+            color: black;
+            text-align: justify;
+        }
 
+        .font-black {
+            color: black;
+        }
+
+        .margin_left_45 {
+            margin-left: 45px;
+        }
+
+        .sub-details li {
+            font-size: 15px;
+        }
+    </style>
+@endsection
 @section('content')
     @includeIf('frontend.partials.global.common-header')
 
     <div class="container">
         <div class="header m-3 p-3">
-            <h3 class="heading ">Partner boarding: </h3>
-            <p class="heading-details">Partnering with other businesses and joining with more sales agent becomes most easier
-                work with
-                Unipuller. Partnership and boarding more sales agents are not hard task anymore. Hundreds of sales
-                agents can join you instantly if you just simply have products and services.</p>
 
-            <div class="row">
-                <div class="col-md-7 m-auto text-center">
-                    <p> Join as partner </p>
-                    <a href="{{ route('login') }}" target="__blank">Click here</a>
-                </div>
-                <div class="col-md-5 mt-5">
-                    <img src="{{ asset('assets/frontend/partner-boarding.png') }}" alt="digital-marketing.png"
-                        style="width: 100%">
+            <div class="welcome">
+                <h4 class="fw-bold">
+                    Revolutionizing Supply Chain Partnerships with Unipuller's Innovative Solutions
+                </h4>
+            </div>
+
+
+
+
+            <div class="why_choose_uit mt-4">
+                <h5 class="fw-bold">
+                    1. Streamlined Partnership Formation
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Supply chain companies can now forge partnerships effortlessly, thanks to
+                        Unipuller's advanced technology. By streamlining the partnership formation process, we help supply
+                        chain businesses access new opportunities and expand their network of partners. This efficiency is
+                        crucial for ensuring a resilient and agile supply chain.
+                    </p>
                 </div>
             </div>
-        </div>
 
-        <div class="body m-3 p-3">
-            <h6 class="body-heading ">Join as sales agent :</h6>
+            <div class="why_choose_uit mt-4">
+                <h5 class="fw-bold">
+                    2. Instant Sales Agent Onboarding
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; For supply chain companies, the ability to onboard sales agents swiftly is a
+                        game-changer. Unipuller's technology-focused solution removes the time-consuming and often arduous
+                        aspects of recruiting and onboarding. With a few clicks, companies can add sales agents, enhancing
+                        their distribution reach and market presence. This agility is essential in adapting to changing
+                        market demands.
+                    </p>
+                </div>
+            </div>
 
-            <h6 class="body-footer mb-5">To be sales agent you just need to join with Unipuller. Contacting with companies,
-                doing negotiations
-                and making agreement for partnership is obsolete in Unipuller. With us, you just choose your preferred
-                company and click on join button. Instantly you’ll become sales partner of that company while Unipuller
-                will maintain all the process on behalf of you</h6>
-
+            <div class="why_choose_uit mt-4">
+                <h5 class="fw-bold">
+                    3. Hassle-Free Partnership Management
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Managing a complex network of partners can be overwhelming for supply chain
+                        companies. Unipuller's technology simplifies partnership management, ensuring smooth operations and
+                        reducing administrative burdens. This translates to enhanced efficiency in the supply chain, better
+                        communication with partners, and faster response times to market fluctuations.
+                    </p>
+                </div>
+            </div>
+            <div class="why_choose_uit mt-4">
+                <h5 class="fw-bold">
+                    3. Accessible Business Growth
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller empowers supply chain companies to explore new growth opportunities
+                        and diversify their services. Our technology-driven approach opens doors to expansion, offering
+                        benefits such as increased market reach and the ability to tap into new customer segments. In a
+                        competitive supply chain landscape, this accessible business growth is vital for staying ahead of
+                        the curve.
+                    </p>
+                </div>
+            </div>
+            <p>
+                In summary, Unipuller's technological solutions are tailored to meet the specific needs of supply chain
+                companies. By simplifying partnership processes, onboarding, and management, we enable these businesses to
+                adapt and thrive in an ever-evolving market. The result is a more resilient, agile, and growth-focused
+                supply chain ecosystem.
+            </p>
         </div>
     </div>
 @endsection
