3d57b22f3813dc76178b488bc1eb3a44b565efa3
Golam Rahman Sagor <grsagor08@gmail.com>
2023-10-30 23:46:13 +0600

    push

---
+++ resources/views/frontend/footerDetails/make-money/makeMoney.blade.php
@@ -0,0 +1,444 @@
+<div class="tab-pane fade show active {{ Route::is('footer.details.make.money') ? '' : 'd-none' }}"
+    id="nav-sell-on-unipuller" role="tabpanel" aria-labelledby="nav-sell-on-unipuller-tab" tabindex="0">
+    <h3 class="footer-details-title">Sell on unipuller</h3>
+    <div class="header m-3 p-3">
+
+        <div class="welcome">
+            <h4 class="fw-bold">
+                Unipuller: Your Gateway to Business Partnerships and Growth
+            </h4>
+            <p>
+                &nbsp; &nbsp; &nbsp; Are you looking to expand your business, establish partnerships, and
+                accelerate
+                your growth without the need for substantial investment, extensive knowledge, or a significant
+                time
+                commitment? Unipuller is here to transform your business journey with our comprehensive
+                consultancy
+                services and innovative Unipuller Business Solution technology.
+            </p>
+        </div>
+
+        <div class="welcome">
+            <h5 class="fw-bold">
+                Why Choose Unipuller?
+            </h5>
+
+            <div class="why_choose_uit mt-2">
+                <h5 class="fw-bold">
+                    1. Partnership Made Easy:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller specializes in bringing businesses together under our
+                        partnership
+                        network. We simplify the complex process of finding and forming partnerships, saving you
+                        valuable time and resources.
+
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    2. Partnership:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; With Unipuller, you can connect with sales agents and partners
+                        effortlessly. Our platform enables smooth communication and collaboration, ensuring that
+                        your
+                        business thrives through effective partnerships.
+
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    3. Low Barriers to Entry:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller's revolutionary approach to business allows anyone to
+                        start a
+                        business venture with minimal investment and prior knowledge. We've eliminated the
+                        traditional
+                        obstacles that often hinder aspiring entrepreneurs.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    4. Cutting-Edge Technology:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our Unipuller Business Solution technology is at the heart of our
+                        success.
+                        This innovative platform empowers users with the tools they need to grow their
+                        businesses
+                        efficiently. From managing partnerships to accessing valuable resources, our technology
+                        has you
+                        covered.
+
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="welcome">
+            <h5 class="fw-bold">
+                Services Offered by Unipuller
+            </h5>
+
+            <div class="why_choose_uit mt-2">
+                <h5 class="fw-bold">
+                    1. Business Consultancy:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our experienced consultants are ready to guide you through the
+                        process of
+                        forming successful partnerships and expanding your business. Whether you're a seasoned
+                        entrepreneur or new to the business world, we provide tailored advice to suit your
+                        needs.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    2. Partner Matching:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller excels in pairing your business with the right partners
+                        to help
+                        you thrive. Our network includes a diverse range of sales agents and businesses, making
+                        it easy
+                        to find the perfect fit for your goals.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    3. Training and Support:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller offers comprehensive training and ongoing support to help
+                        you
+                        make the most of your partnerships. We ensure you have the tools and knowledge you need
+                        to
+                        succeed.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    4. Unipuller Business Solution Technology:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our user-friendly platform is designed to simplify and streamline
+                        your
+                        business operations. Manage partnerships, access resources, and grow your business with
+                        ease,
+                        all in one place.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <h5 class="fw-bold">
+            2. Get Started with Unipuller Today!
+        </h5>
+        <div class="details">
+            <p>
+                &nbsp; &nbsp; &nbsp; Unipuller is your key to unlocking business growth and forming valuable
+                partnerships. Whether you're a seasoned business owner or just starting your entrepreneurial
+                journey,
+                we're here to make it easier for you. Join Unipuller, and let's shape a more prosperous future
+                for your
+                business, together.
+
+            </p>
+        </div>
+
+        <p>
+            Ready to get started? Visit our website, contact our team, and embark on a new journey of business
+            success
+            with Unipuller today.
+        </p>
+    </div>
+</div>
+
+<div class="tab-pane fade" id="nav-sell-on-unipuller-technology" role="tabpanel"
+    aria-labelledby="nav-sell-on-unipuller-technology-tab" tabindex="0">
+    <h3 class="footer-details-title">Sell on unipuller technology</h3>
+    <div class="header m-3 p-3">
+
+        <div class="welcome">
+            <h4 class="fw-bold">
+                Unipuller: Where Your Business Partnerships Thrive and Growth Knows No Limits
+            </h4>
+            <p>
+                &nbsp; &nbsp; &nbsp; Are you looking to expand your business, establish partnerships, and
+                accelerate
+                your growth without the need for substantial investment, extensive knowledge, or a significant
+                time
+                commitment? Unipuller is here to transform your business journey with our comprehensive
+                consultancy
+                services and innovative Unipuller Business Solution technology.
+            </p>
+        </div>
+
+        <div class="welcome">
+            <h5 class="fw-bold">
+                Why Choose Unipuller?
+            </h5>
+
+            <div class="why_choose_uit mt-2">
+                <h5 class="fw-bold">
+                    1. Partnership Made Easy:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller specializes in bringing businesses together under our
+                        partnership
+                        network. We simplify the complex process of finding and forming partnerships, saving you
+                        valuable time and resources.
+
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    2. Partnership:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; With Unipuller, you can connect with sales agents and partners
+                        effortlessly. Our platform enables smooth communication and collaboration, ensuring that
+                        your
+                        business thrives through effective partnerships.
+
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    3. Low Barriers to Entry:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller's revolutionary approach to business allows anyone to
+                        start a
+                        business venture with minimal investment and prior knowledge. We've eliminated the
+                        traditional
+                        obstacles that often hinder aspiring entrepreneurs.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    4. Cutting-Edge Technology:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our Unipuller Business Solution technology is at the heart of our
+                        success.
+                        This innovative platform empowers users with the tools they need to grow their
+                        businesses
+                        efficiently. From managing partnerships to accessing valuable resources, our technology
+                        has you
+                        covered.
+
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="welcome">
+            <h5 class="fw-bold">
+                Services Offered by Unipuller
+            </h5>
+
+            <div class="why_choose_uit mt-2">
+                <h5 class="fw-bold">
+                    1. Business Consultancy:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our experienced consultants are ready to guide you through the
+                        process of
+                        forming successful partnerships and expanding your business. Whether you're a seasoned
+                        entrepreneur or new to the business world, we provide tailored advice to suit your
+                        needs.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    2. Partner Matching:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller excels in pairing your business with the right partners
+                        to help
+                        you thrive. Our network includes a diverse range of sales agents and businesses, making
+                        it easy
+                        to find the perfect fit for your goals.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    3. Training and Support:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller offers comprehensive training and ongoing support to help
+                        you
+                        make the most of your partnerships. We ensure you have the tools and knowledge you need
+                        to
+                        succeed.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    4. Unipuller Business Solution Technology:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our user-friendly platform is designed to simplify and streamline
+                        your
+                        business operations. Manage partnerships, access resources, and grow your business with
+                        ease,
+                        all in one place.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <h5 class="fw-bold">
+            2. Get Started with Unipuller Today!
+        </h5>
+        <div class="details">
+            <p>
+                &nbsp; &nbsp; &nbsp; Unipuller is your key to unlocking business growth and forming valuable
+                partnerships. Whether you're a seasoned business owner or just starting your entrepreneurial
+                journey,
+                we're here to make it easier for you. Join Unipuller, and let's shape a more prosperous future
+                for your
+                business, together.
+
+            </p>
+        </div>
+
+        <p>
+            Ready to get started? Visit our website, contact our team, and embark on a new journey of business
+            success
+            with Unipuller today.
+        </p>
+    </div>
+</div>
+
+<div class="tab-pane fade" id="nav-associate-program" role="tabpanel" aria-labelledby="nav-associate-program-tab"
+    tabindex="0">
+    <h3 class="footer-details-title">Associate program</h3>
+    <p class="footer-details-content">Content</p>
+</div>
+
+<div class="tab-pane fade" id="nav-delivery-partner" role="tabpanel" aria-labelledby="nav-delivery-partner-tab"
+    tabindex="0">
+    <h3 class="footer-details-title">Delivery partnership</h3>
+    <div class="header m-3 p-3">
+
+        <div class="welcome">
+            <h4 class="fw-bold">
+                Service Delivery Partnership with Unipuller: Elevate Your Business to New Heights
+            </h4>
+            <p>
+                &nbsp; &nbsp; &nbsp; At Unipuller, we believe in the power of collaboration and mutual growth.
+                Our
+                Service Delivery Partnership program is designed to empower businesses like yours to soar to new
+                heights. Discover the key benefits of partnering with Unipuller
+            </p>
+        </div>
+
+        <div class="welcome">
+
+            <div class="why_choose_uit mt-2">
+                <h5 class="fw-bold">
+                    1. Enhanced Service Capabilities:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; When you become a Unipuller Service Delivery Partner, you gain
+                        access to a
+                        wide array of services and resources. This includes cutting-edge technology, industry
+                        expertise,
+                        and a diverse range of solutions that can elevate your service offerings to your
+                        clients.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    2. Business Expansion:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller is your key to expanding your business horizons. As a
+                        Service
+                        Delivery Partner, you tap into our vast network of clients and partners, opening doors
+                        to new
+                        markets and growth opportunities that were previously beyond your reach.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    3. Streamlined Collaboration:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Our platform provides a seamless environment for communication,
+                        project
+                        management, and partnership building. It's designed to simplify the process of working
+                        with
+                        Unipuller and other partners, making collaboration a breeze.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    4. Shared Success:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller's Service Delivery Partnership program is founded on the
+                        principle of shared success. Your accomplishments are celebrated as our achievements,
+                        and we're
+                        dedicated to supporting your growth at every step of the journey.
+                    </p>
+                </div>
+
+                <h5 class="fw-bold">
+                    5. Service Diversity:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; By partnering with Unipuller, you diversify your service portfolio.
+                        You'll
+                        have the opportunity to offer a wider range of services to your clients, enhancing your
+                        value
+                        proposition and meeting the ever-evolving needs of your customers.
+                    </p>
+                </div>
+
+                <h5 class="fw-bold">
+                    6. Industry Insights:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Unipuller offers valuable industry insights and expertise. As a
+                        partner,
+                        you'll benefit from our collective knowledge, helping you stay ahead of industry trends
+                        and make
+                        informed decisions for your business.
+                    </p>
+                </div>
+                <h5 class="fw-bold">
+                    7. Mutual Growth:
+                </h5>
+                <div class="details">
+                    <p>
+                        &nbsp; &nbsp; &nbsp; Your growth is our success. Unipuller is committed to nurturing
+                        Service
+                        Delivery Partners and ensuring they reach their full potential. We view our partners as
+                        an
+                        integral part of our ecosystem, and together, we'll thrive.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+        <p>
+            Unipuller's Service Delivery Partnership is more than a collaboration; it's a strategic alliance for
+            businesses looking to enhance their service offerings, expand their reach, and achieve greater
+            success. Join
+            us in redefining the future of service delivery and let Unipuller be your partner in growth.
+        </p>
+    </div>
+</div>
---
+++ resources/views/frontend/footerDetails/partial/style.blade.php
@@ -0,0 +1,59 @@
+<style>
+    p,
+    li {
+        color: black;
+        text-align: justify;
+    }
+
+    .font-black {
+        color: black;
+    }
+
+    .margin_left_45 {
+        margin-left: 45px;
+    }
+
+    .sub-details li {
+        font-size: 15px;
+    }
+
+    .footer-details-container {
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+        gap: 20px;
+        padding: 0 20px;
+        margin-top: 20px;
+    }
+
+    .footer-details-sidebar {
+        border: 1px solid #ddd;
+        padding: 20px !important;
+    }
+
+    .nav-tabs .nav-item.show .nav-link,
+    .nav-tabs .nav-link.active {
+        color: #495057;
+        background-color: #fff;
+        border: none !important;
+    }
+
+    .nav-link {
+        padding: 0 !important;
+    }
+
+    .footer-details-content {
+        padding: 20px !important;
+        border: 1px solid #ddd;
+    }
+
+    .footer-details-content {}
+
+    .footer-details-title {}
+</style>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="your-integrity-hash-goes-here" crossorigin="anonymous">
+
+
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> --}}
