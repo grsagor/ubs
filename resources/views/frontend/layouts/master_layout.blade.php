9f270e344917757e0597fd056f8dd74277dfd5aa
Golam Rahman Sagor <grsagor08@gmail.com>
2023-11-01 03:29:15 +0600

    push

--- resources/views/frontend/layouts/master_layout.blade.php
+++ resources/views/frontend/layouts/master_layout.blade.php
@@ -40,7 +40,7 @@
 
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/logo.png') }}" />
     <!-- Google Font -->
-    <!-- 31-10-2023 -->
+    <!-- 3:29 AM -->
     {{-- @if ($default_font->font_value)
         <link href="https://fonts.cdnfonts.com/css/amazon-ember" rel="stylesheet">
 
