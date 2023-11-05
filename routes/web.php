70e767da598b58adfd7758aecb4284b7ff294def
Abdulla-Shakib <abshakib91@gmail.com>
2023-11-05 20:54:22 +0600

    Mail basic

--- routes/web.php
+++ routes/web.php
@@ -110,6 +110,18 @@ Route::get('/route-optimize-clear', function () {
     return '<h2>Events, views, cache, route, config, compiled clear</h2>';
 });
 
+Route::get('send-mail', function () {
+
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+
+    \Mail::to('abshakib91@gmail.com')->send(new \App\Mail\MyTestMail($details));
+
+    dd("Email is Sent.");
+});
+
 // Stripe payment gateway
 Route::controller(StripePaymentController::class)->group(function () {
     Route::get('stripe', 'stripe');
