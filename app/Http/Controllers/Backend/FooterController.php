0946fc4b07fc48c28a1dccc9d6a93f737775941a
Abdulla-Shakib <abshakib91@gmail.com>
2023-12-17 19:29:14 +0600

    Footer finish 4

--- app/Http/Controllers/Backend/FooterController.php
+++ app/Http/Controllers/Backend/FooterController.php
@@ -11,42 +11,63 @@ class FooterController extends Controller
 {
     public function index(Request $request)
     {
+        if (auth()->user()->id != 5) {
+            // abort(403, 'Unauthorized action.');
+            $output = [
+                'success' => False,
+                'msg' => 'You are not allowed',
+            ];
+            return redirect()->back()->with('status', $output);
+        }
         $data = Footer::query()->get();
         return view('backend.footer.index', compact('data'));
     }
 
     public function create()
     {
-        return view('backend.footer.create');
+        if (auth()->user()->id != 5) {
+            abort(403, 'Unauthorized action.');
+        }
+        // return view('backend.footer.create');
     }
 
     public function store(Request $request)
     {
-        try {
-            $requestData = $request->all();
-            $footer = new Footer();
-            $footer->description = $requestData['description'];
-            $footer->save();
-            $output = [
-                'success' => true,
-                'msg' => ('Created Successfully!!!'),
-            ];
-
-            return redirect()->back()->with('status', $output);
-        } catch (\Throwable $e) {
-            dd($e->getmessage());
-            return redirect()->back();
+        if (auth()->user()->id != 5) {
+            abort(403, 'Unauthorized action.');
         }
+        // try {
+        //     $requestData = $request->all();
+        //     $footer = new Footer();
+        //     $footer->description = $requestData['description'];
+        //     $footer->save();
+        //     $output = [
+        //         'success' => true,
+        //         'msg' => ('Created Successfully!!!'),
+        //     ];
+
+        //     return redirect()->back()->with('status', $output);
+        // } catch (\Throwable $e) {
+        //     dd($e->getmessage());
+        //     return redirect()->back();
+        // }
     }
 
     public function edit($id)
     {
+        if (auth()->user()->id != 5) {
+            abort(403, 'Unauthorized action.');
+        }
         $footer = Footer::find($id);
         return view('backend.footer.edit', compact('footer'));
     }
 
     public function update(Request $request, $id)
     {
+        if (auth()->user()->id != 5) {
+            abort(403, 'Unauthorized action.');
+        }
+
         try {
             $validatedData = $request->validate([
                 'description' => 'required',
--- routes/web.php
+++ routes/web.php
@@ -263,11 +263,11 @@ Route::middleware(['setData'])->group(function () {
 // Route::middleware(['setData', 'auth', 'SetSessionData', 'language', 'timezone', 'AdminSidebarMenu', 'CheckUserLogin'])->group(function () {
 Route::middleware(['checkAdmin', 'SetSessionData'])->group(function () {
 
-    Route::get('/footer', [FooterController::class, 'index'])->name('footer.index');
-    Route::get('/footer/create', [FooterController::class, 'create'])->name('footer.create');
-    Route::post('/footer', [FooterController::class, 'store'])->name('footer.store');
-    Route::get('/footer/{id}/edit', [FooterController::class, 'edit'])->name('footer.edit');
-    Route::put('/footer/{id}', [FooterController::class, 'update'])->name('footer.update');
+    Route::get('/footer',                  [FooterController::class, 'index'])->name('footer.index');
+    Route::get('/footer/create',           [FooterController::class, 'create'])->name('footer.create');
+    Route::post('/footer',                 [FooterController::class, 'store'])->name('footer.store');
+    Route::get('/footer/{id}/edit',        [FooterController::class, 'edit'])->name('footer.edit');
+    Route::put('/footer/{id}',             [FooterController::class, 'update'])->name('footer.update');
 
     // Services
     Route::resource('service-advertise', ServiceAdvertiseRoomController::class);
