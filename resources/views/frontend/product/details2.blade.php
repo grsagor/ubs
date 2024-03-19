2728ca40b0ce006d7bb94e6ece50154515f1815d
Abdulla-Shakib <abshakib91@gmail.com>
2024-03-20 00:19:48 +0600

    Mobile view changes

--- resources/views/frontend/product/details2.blade.php
+++ resources/views/frontend/product/details2.blade.php
@@ -189,9 +189,9 @@
         }
 
         /* .carousel-control-next,
-                                                                                                                            .carousel-control-prev {
-                                                                                                                                filter: invert(100%);
-                                                                                                                            } */
+                                                                                                                                        .carousel-control-prev {
+                                                                                                                                            filter: invert(100%);
+                                                                                                                                        } */
 
         .carousel-control-prev-icon {
             background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' width='8' height='8' viewBox='0 0 8 8'%3e%3cpath d='M5.25 0l-4 4 4 4 1.5-1.5L4.25 4l2.5-2.5L5.25 0z'/%3e%3c/svg%3e");
@@ -236,6 +236,10 @@
 
             .mobile_image {
                 margin: 10px 0px 10px 0px !important;
+            }
+
+            .mobile_m_15 {
+                margin-top: 15px !important;
             }
 
         }
@@ -469,7 +473,7 @@
                                                 @endif
 
                                                 @if ($info->image)
-                                                    <div class="col-md-6" style="margin: auto;">
+                                                    <div class="col-md-6 mobile_m_15" style="margin: auto;">
                                                         <div id="imageSlider" class="carousel slide"
                                                             data-bs-ride="carousel">
                                                             <div class="carousel-inner">
