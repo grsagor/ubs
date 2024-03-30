4079e7300105ea84a12925688babadd98cb8d8dc
Abdulla-Shakib <abshakib91@gmail.com>
2024-03-30 22:56:20 +0600

    Pdf show in service details page

--- resources/views/frontend/product/details2.blade.php
+++ resources/views/frontend/product/details2.blade.php
@@ -182,16 +182,16 @@
 
         #imageSlider .carousel-item img {
             /* max-width: 350px;
-                            max-height: 300px; */
+                                max-height: 300px; */
             width: auto;
             height: auto;
             margin: auto;
         }
 
         /* .carousel-control-next,
-                                                                                                                                                                                .carousel-control-prev {
-                                                                                                                                                                                    filter: invert(100%);
-                                                                                                                                                                                } */
+                                                                                                                                                                                    .carousel-control-prev {
+                                                                                                                                                                                        filter: invert(100%);
+                                                                                                                                                                                    } */
 
         .carousel-control-prev-icon {
             background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' width='8' height='8' viewBox='0 0 8 8'%3e%3cpath d='M5.25 0l-4 4 4 4 1.5-1.5L4.25 4l2.5-2.5L5.25 0z'/%3e%3c/svg%3e");
@@ -558,8 +558,6 @@
                                                                     </div>
                                                                 @endif
                                                             </div>
-
-
 
                                                         </div>
                                                     </div>
