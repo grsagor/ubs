ccea1c51b7a6bbb587b9c0e87fbc3746e72dd037
Abdulla-Shakib <abshakib91@gmail.com>
2024-04-08 16:02:36 +0600

    No product found correction

--- resources/views/frontend/product/partial/app.blade.php
+++ resources/views/frontend/product/partial/app.blade.php
@@ -29,7 +29,7 @@
 
         p.category_text {
             /* min-height: 20px;
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    max-height: 20px; */
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        max-height: 20px; */
         }
 
         h5.product-title {
@@ -111,7 +111,7 @@
                                                                 @elseif (!empty($childCategory['children']))
                                                                     class="toggle-category" @endif>
                                                                     <span
-                                                                        class="toggle-icon{{ Route::currentRouteName() === 'service.list' && request()->sub_category_id == $childCategory['id'] ? 'text-danger' : '' }} toggle-icon">
+                                                                        class="toggle-icon {{ Route::currentRouteName() === 'service.list' && request()->sub_category_id == $childCategory['id'] ? 'text-danger' : '' }} toggle-icon">
                                                                         &nbsp;{{ $childCategory['name'] }}
                                                                         @if (!empty($childCategory['children']))
                                                                             <svg xmlns="http://www.w3.org/2000/svg"
@@ -245,74 +245,73 @@
 
                                 <div class="row mb-4 g-3 product-style-1 shop-list product-list  e-title-hover-primary e-hover-image-zoom"
                                     style="padding: 0px !important;">
-                                    @if (count($products) > 0)
-                                        <div class="col-md-9 laptop_view_card mobile_view_card">
+                                    <div class="col-md-9 laptop_view_card mobile_view_card">
+                                        @if (count($products) > 0)
                                             @yield('property_list_content')
-                                        </div>
-
-
-                                        {{-- Right side Advertise widget --}}
-                                        <div class="col-md-3">
-
+                                        @else
                                             <div class="card">
                                                 <div class="card-body">
-                                                    <h5 class="card-title">Advertise your service</h5>
-                                                    <p class="card-text">List your service unlimited and completely free.
-                                                    </p>
-                                                    <a href="{{ route('products.create') }}" class="button-31">Add</a>
+                                                    <div class="page-center">
+                                                        <h4 class="text-center text-danger">{{ 'No Product Found.' }}</h4>
+                                                    </div>
                                                 </div>
                                             </div>
+                                        @endif
+                                    </div>
 
-                                            <br>
+                                    {{-- Right side Advertise widget --}}
+                                    <div class="col-md-3">
 
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <h5 class="card-title">Can't find your service?</h5>
-                                                    <p class="card-text">Contact us to get what you need. We will connect
-                                                        you with the right service provider who knows that solution.</p>
-                                                    <a href="#" class="button-31">Add</a>
-                                                </div>
-                                            </div>
-
-                                            <br>
-
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <h5 class="card-title">Software to run your business?</h5>
-                                                    <p class="card-text">Use our complete business solution software package
-                                                        completely free of charge. The most updated and latest technology to
-                                                        serve your business needs.</p>
-                                                    <a href="{{ url('business/register') }}" class="button-31">Add</a>
-                                                </div>
-                                            </div>
-
-                                            <br>
-
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <h5 class="card-title">Find your favourite company.</h5>
-                                                    <ol>
-                                                        <li class="card-text">Company wise filter
-                                                        </li>
-                                                        <li class="card-text">Search by location
-                                                        </li>
-                                                    </ol>
-                                                    <a href="{{ route('shop.list') }}" class="button-31">Details</a>
-                                                </div>
-                                            </div>
-
-                                            <br>
-
-                                        </div>
-                                    @else
                                         <div class="card">
                                             <div class="card-body">
-                                                <div class="page-center">
-                                                    <h4 class="text-center">{{ 'No Product Found.' }}</h4>
-                                                </div>
+                                                <h5 class="card-title">Advertise your service</h5>
+                                                <p class="card-text">List your service unlimited and completely free.
+                                                </p>
+                                                <a href="{{ route('products.create') }}" class="button-31">Add</a>
                                             </div>
                                         </div>
-                                    @endif
+
+                                        <br>
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Can't find your service?</h5>
+                                                <p class="card-text">Contact us to get what you need. We will connect
+                                                    you with the right service provider who knows that solution.</p>
+                                                <a href="#" class="button-31">Add</a>
+                                            </div>
+                                        </div>
+
+                                        <br>
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Software to run your business?</h5>
+                                                <p class="card-text">Use our complete business solution software package
+                                                    completely free of charge. The most updated and latest technology to
+                                                    serve your business needs.</p>
+                                                <a href="{{ url('business/register') }}" class="button-31">Add</a>
+                                            </div>
+                                        </div>
+
+                                        <br>
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Find your favourite company.</h5>
+                                                <ol>
+                                                    <li class="card-text">Company wise filter
+                                                    </li>
+                                                    <li class="card-text">Search by location
+                                                    </li>
+                                                </ol>
+                                                <a href="{{ route('shop.list') }}" class="button-31">Details</a>
+                                            </div>
+                                        </div>
+
+                                        <br>
+
+                                    </div>
                                 </div>
 
                             </div>
