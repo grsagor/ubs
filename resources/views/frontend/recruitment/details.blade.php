4fb31b329f315c4e5412bb3528807f9c3b4c0e3a
Abdulla-Shakib <abshakib91@gmail.com>
2024-07-30 15:12:43 +0600

    Jobs details page

--- resources/views/frontend/recruitment/details.blade.php
+++ resources/views/frontend/recruitment/details.blade.php
@@ -1,172 +1,9 @@
 @extends('frontend.layouts.master_layout')
 @section('title', $job->title)
 @section('css')
-    <style>
-        .color-black {
-            color: black !important;
-        }
-
-        .text-justify {
-            text-align: justify;
-        }
-
-        .btn {
-            line-height: 33px !important;
-            padding: 0 20px !important;
-        }
-
-        .card-design {
-            background-color: #fdfdfd;
-            color: #212529;
-            /* Dark text color */
-        }
-
-        .job-title {
-            color: #333;
-            font-size: 18px;
-            font-weight: 600;
-        }
-
-        .deadline-heading {
-            color: #333;
-            font-size: 14px;
-            font-weight: 400;
-        }
-
-        .deadline-date {
-            color: #B32D7D;
-            font-size: 14px;
-            font-weight: 600;
-        }
-
-        .apply-section {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .deadline-heading {
-            flex: 1;
-        }
-
-        .applynow {
-            border-radius: 4px;
-            background: #008020;
-            color: #FFF;
-            font-weight: 500;
-        }
-
-        .alreadyApplied {
-            border-radius: 4px;
-            background: #c9030f;
-            color: #FFF;
-            font-weight: 500;
-        }
-
-        .btn-no {
-            border-radius: 4px;
-            background: #c9030f;
-            color: #FFF;
-            font-weight: 500;
-        }
-
-        .summery-card,
-        .company-info-card {
-            border-radius: 4px;
-            border: 0.5px solid #DDD;
-            background: #F4F4F4;
-            display: flex;
-            flex-direction: column;
-            padding: 15px 8px;
-        }
-
-        .requirements-card {
-            border-radius: 4px;
-            border: 0.5px solid #bababa;
-            background: #fcfcfc;
-            display: flex;
-            flex-direction: column;
-            padding: 15px 8px;
-        }
-
-        .sectitle {
-            color: #B32D7D;
-            font-size: 16px !important;
-            font-weight: 600;
-            margin-bottom: 10px;
-        }
-
-        .txtbold {
-            font-weight: 600;
-        }
-
-        .subheading {
-            color: #333;
-            font-size: 14px;
-            font-weight: 600;
-        }
-
-        .reptitle {
-            color: #B83835;
-            font-size: 18px;
-            font-weight: 600;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .report-card {
-            border-radius: 4px;
-            border: 0.5px solid #FDB5B3;
-            background: #FFEFEF;
-            padding: 10px;
-        }
-
-        .report-button {
-            background-color: #e43200e2;
-            color: #fff;
-            padding: 5px 10px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .complain-info-item {
-            display: flex;
-            align-items: center;
-        }
-
-        .complain-info-item i {
-            margin-right: 5px;
-        }
-
-        .mobile-view {
-            display: none;
-        }
-
-        .description ul li {
-            list-style: disc inside;
-        }
-
-        .description ul {
-            margin-bottom: 15px;
-        }
-
-        @media (max-width: 767px) {
-            .reptitle {
-                font-size: 15px !important;
-            }
-
-            .mobile-view {
-                display: block;
-            }
-
-            .laptopp-view {
-                display: none;
-            }
-        }
-    </style>
+    @include('frontend.recruitment.partial.details_css')
 @endsection
+
 @section('content')
     @includeIf('frontend.partials.global.common-header')
 
@@ -233,10 +70,9 @@
                                     class="deadline-date">{{ \Carbon\Carbon::parse($job->closing_date)->format('d F Y') }}</span>
                             </div>
                             <div class="apply-button">
-
                                 @include('frontend.recruitment.applyBtn')
-
                             </div>
+
                         </div>
 
                         <div class="summary-section row mt-3">
@@ -257,13 +93,24 @@
                                                 Salary: <span class="txtbold">
                                                     @if ($job->salary)
                                                         {{ $job->salary }}/{{ $job->salary_type }}
-                                                    @else
-                                                        {{ $job->salary_type }}
+                                                    @elseif ($job->salary_type == 'Negotiable')
+                                                        {{ $job->from_salary }} - {{ $job->to_salary }}
+                                                    @elseif ($job->salary_type == 'Fixed')
+                                                        {{ $job->fixed_salary }}
                                                     @endif
                                                 </span>
                                             </div>
 
+                                            @if ($job->vacancies)
+                                                <div>
+                                                    Vacancies: <span class="txtbold">
+                                                        {{ $job->vacancies }}
+                                                    </span>
+                                                </div>
+                                            @endif
+
                                         </div>
+
                                         <div class="col-md-6">
                                             <div>
                                                 Job Location: <span class="txtbold">{{ $job->location }}</span>
---
+++ resources/views/frontend/recruitment/partial/details_css.blade.php
@@ -0,0 +1,165 @@
+<style>
+    .color-black {
+        color: black !important;
+    }
+
+    .text-justify {
+        text-align: justify;
+    }
+
+    .btn {
+        line-height: 33px !important;
+        padding: 0 20px !important;
+    }
+
+    .card-design {
+        background-color: #fdfdfd;
+        color: #212529;
+        /* Dark text color */
+    }
+
+    .job-title {
+        color: #333;
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    .deadline-heading {
+        color: #333;
+        font-size: 14px;
+        font-weight: 400;
+    }
+
+    .deadline-date {
+        color: #B32D7D;
+        font-size: 14px;
+        font-weight: 600;
+    }
+
+    .apply-section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .deadline-heading {
+        flex: 1;
+    }
+
+    .applynow {
+        border-radius: 4px;
+        background: #008020;
+        color: #FFF;
+        font-weight: 500;
+    }
+
+    .alreadyApplied {
+        border-radius: 4px;
+        background: #c9030f;
+        color: #FFF;
+        font-weight: 500;
+    }
+
+    .btn-no {
+        border-radius: 4px;
+        background: #c9030f;
+        color: #FFF;
+        font-weight: 500;
+    }
+
+    .summery-card,
+    .company-info-card {
+        border-radius: 4px;
+        border: 0.5px solid #DDD;
+        background: #F4F4F4;
+        display: flex;
+        flex-direction: column;
+        padding: 15px 8px;
+    }
+
+    .requirements-card {
+        border-radius: 4px;
+        border: 0.5px solid #bababa;
+        background: #fcfcfc;
+        display: flex;
+        flex-direction: column;
+        padding: 15px 8px;
+    }
+
+    .sectitle {
+        color: #B32D7D;
+        font-size: 16px !important;
+        font-weight: 600;
+        margin-bottom: 10px;
+    }
+
+    .txtbold {
+        font-weight: 600;
+    }
+
+    .subheading {
+        color: #333;
+        font-size: 14px;
+        font-weight: 600;
+    }
+
+    .reptitle {
+        color: #B83835;
+        font-size: 18px;
+        font-weight: 600;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .report-card {
+        border-radius: 4px;
+        border: 0.5px solid #FDB5B3;
+        background: #FFEFEF;
+        padding: 10px;
+    }
+
+    .report-button {
+        background-color: #e43200e2;
+        color: #fff;
+        padding: 5px 10px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .complain-info-item {
+        display: flex;
+        align-items: center;
+    }
+
+    .complain-info-item i {
+        margin-right: 5px;
+    }
+
+    .mobile-view {
+        display: none;
+    }
+
+    .description ul li {
+        list-style: disc inside;
+    }
+
+    .description ul {
+        margin-bottom: 15px;
+    }
+
+    @media (max-width: 767px) {
+        .reptitle {
+            font-size: 15px !important;
+        }
+
+        .mobile-view {
+            display: block;
+        }
+
+        .laptopp-view {
+            display: none;
+        }
+    }
+</style>
