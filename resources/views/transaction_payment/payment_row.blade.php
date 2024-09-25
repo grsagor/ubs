a9dd042c6b28867f27c6ebdd540c0ebabb6f777f
Golam Rahman Sagor <grsagor08@gmail.com>
2024-09-25 07:58:55 +0600

    all done

--- resources/views/transaction_payment/payment_row.blade.php
+++ resources/views/transaction_payment/payment_row.blade.php
@@ -98,11 +98,11 @@
 
         @php
             $pos_settings = !empty(session()->get('business.pos_settings')) ? json_decode(session()->get('business.pos_settings'), true) : [];
-            $enable_cash_denomination_for_payment_methods = !empty($pos_settings['enable_cash_denomination_for_payment_methods']) ? $pos_settings['enable_cash_denomination_for_payment_methods'] : [];
+            $enable_cash_denomination_for_payment_methods = isset($pos_settings['enable_cash_denomination_for_payment_methods']) && !empty($pos_settings['enable_cash_denomination_for_payment_methods']) ? $pos_settings['enable_cash_denomination_for_payment_methods'] : [];
         @endphp
 
         @if(!empty($pos_settings['enable_cash_denomination_on']) && $pos_settings['enable_cash_denomination_on'] == 'all_screens')
-            <input type="hidden" class="enable_cash_denomination_for_payment_methods" value="{{json_encode($pos_settings['enable_cash_denomination_for_payment_methods'])}}">
+            <input type="hidden" class="enable_cash_denomination_for_payment_methods" value="{{json_encode($enable_cash_denomination_for_payment_methods)}}">
             <div class="clearfix"></div>
             <div class="col-md-12 cash_denomination_div @if(!in_array($payment_line->method, $enable_cash_denomination_for_payment_methods)) hide @endif">
                 <hr>
