edb6bdf0f77ce89cadaf488459489aba4c637f7c
Abdulla-Shakib <abshakib91@gmail.com>
2024-10-01 10:35:19 +0600

    Profile image show 2

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -120,8 +120,9 @@
                         @php
                             $profile_photo = auth()->user()->media;
                         @endphp
-                        <img src="{{ file_exists($profile_photo->file_name) ? asset($profile_photo->file_name) : 'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg' }}"
+                        <img src="{{ $profile_photo ? asset($profile_photo->display_url) : 'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg' }}"
                             class="user-image" alt="User Image">
+
                     </a>
                     <ul class="dropdown-menu">
                         <!-- The user image in the menu -->
